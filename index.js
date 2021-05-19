bdf1812905e776b3fb57a02031e2241f474b9c3d
David Adamashvili <davit.adamashvili848@ens.tsu.edu.ge>
2021-05-19 20:44:56 +0400

    added html

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/public/index.html");
+	//res.sendFile(__dirname + "/public/index.html");
+	res.send("hello, world");
 });
 
 app.listen(port, () => {
