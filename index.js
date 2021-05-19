a1bcac7c271008e9a49115d87226aee4fed17e3b
David Adamashvili <davit.adamashvili848@ens.tsu.edu.ge>
2021-05-20 00:21:42 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const url =
+	"mongodb+srv://datoad4510:2hLRKbEX8u8efDDP@cluster0.hqh9r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(url, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+	console.log("Connected to database!");
+});
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/add_item", (req, res) => {
+	//insert item into database
+	const list_item = req.body;
+	const collection = client.db("database").collection("labeled-sentences");
+	try {
+		collection
+			.insertOne(list_item)
+			.then((data) => {
+				res.send(data.insertedId);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		console.log(`Inserted ${list_item.data}`);
+	} catch (error) {
+		throw error;
+	}
+});
+
+app.get("/get_items", (req, res) => {
+	// pull list items from server
+
+	// get/find all documents in collection
+	client
+		.db("database")
+		.collection("labeled-sentences")
+		.find()
+		.toArray((err, result) => {
+			if (err) {
+				res.send(err);
+			} else {
+				res.send({ collection: result });
+			}
+		});
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ public/browser.js
@@ -0,0 +1,62 @@
+const server = "https://neural-network123.herokuapp.com";
+// const server = "http://localhost:3000";
+
+window.onload = async (event) => {
+	await printDatabase();
+
+	const submit_button = document.querySelector("#submit");
+
+	submit_button.addEventListener("click", async (event) => {
+		const text_input = document.querySelector("#text");
+		const date_button = document.querySelector("#date-button");
+		const balance_button = document.querySelector("#balance-button");
+
+		let label;
+
+		if (date_button.checked) {
+			label = 0;
+		} else if (balance_button.checked) {
+			label = 1;
+		} else {
+			throw error;
+		}
+
+		const text = text_input.value;
+
+		const obj = {
+			text: text,
+			label: label,
+		};
+
+		const id = await insertLabeledSentence(obj);
+		console.log(id);
+	});
+};
+
+// insert todo into mongodb returns id of inserted todo item
+async function insertLabeledSentence(todo) {
+	return fetch(`${server}/add_item`, {
+		method: "POST", // or 'PUT'
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(todo),
+	})
+		.then((response) => response.text())
+		.catch((error) => {
+			console.error("Error:", error);
+		});
+}
+
+async function fetchList() {
+	// get all todos from server
+	return fetch(`${server}/get_items`)
+		.then((res) => res.json())
+		.then((res) => res.collection)
+		.catch((err) => console.log(err));
+}
+
+async function printDatabase() {
+	const db = await fetchList();
+	console.log(db);
+}
