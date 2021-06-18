1f4a82e2d0c19a74cc721b16681955b4044699d1
user1 <54779055+datoad4510@users.noreply.github.com>
2021-06-18 15:13:32 +0400

    fixed javascript

--- public/browser.js
+++ public/browser.js
@@ -2,56 +2,65 @@ const server = "https://neural-network123.herokuapp.com";
 // const server = "http://localhost:3000";
 
 window.onload = async (event) => {
-	await printDatabase();
+    await printDatabase();
 
-	const submit_button = document.querySelector("#submit");
+    const submit_button = document.querySelector(".btnSubmit");
 
-	submit_button.addEventListener("click", async (event) => {
-		const text_input = document.querySelector("#text");
-		const text_input_dva = document.querySelector("#textdva");
-		//const date_button = document.querySelector("#date-button");
-		//const balance_button = document.querySelector("#balance-button");
+    submit_button.addEventListener("click", async (event) => {
+        const first = document.querySelector("#first-question");
+        const second = document.querySelector("#second-question");
+        //const date_button = document.querySelector("#date-button");
+        //const balance_button = document.querySelector("#balance-button");
 
-		const text = text_input.value;
+        const text1 = first.value;
 
-		const obj = {
-			text: text,
-			label: 1,
-		};
+        const label_zero = {
+            text: text1,
+            label: 0,
+        };
+        const text2 = second.value;
 
-		//const id = await insertLabeledSentence(obj);
-		//console.log(id);
+        const label_one = {
+            text: text2,
+            label: 1,
+        };
 
-		text_input.value = "";
-		text_input_dva.value = "";
-		alert("მონაცემები გაიზავნა, მადლობა ^^");
-	});
+        const id1 = await insertLabeledSentence(label_zero);
+        console.log(id1);
+
+        const id2 = await insertLabeledSentence(label_one);
+        console.log(id2);
+
+        first.value = "";
+        second.value = "";
+        alert("მონაცემები გაიზავნა, მადლობა ^^");
+    });
 };
 
 // insert todo into mongodb returns id of inserted todo item
 async function insertLabeledSentence(todo) {
-	return fetch(`${server}/add_item`, {
-		method: "POST", // or 'PUT'
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(todo),
-	})
-		.then((response) => response.text())
-		.catch((error) => {
-			console.error("Error:", error);
-		});
+    return fetch(`${server}/add_item`, {
+        method: "POST", // or 'PUT'
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todo),
+    })
+        .then((response) => response.text())
+        .catch((error) => {
+            console.error("Error:", error);
+        });
 }
 
 async function fetchList() {
-	// get all todos from server
-	return fetch(`${server}/get_items`)
-		.then((res) => res.json())
-		.then((res) => res.collection)
-		.catch((err) => console.log(err));
+    // get all todos from server
+    return fetch(`${server}/get_items`)
+        .then((res) => res.json())
+        .then((res) => res.collection)
+        .catch((err) => console.log(err));
 }
 
 async function printDatabase() {
-	const db = await fetchList();
-	console.log(db);
+    const db = await fetchList();
+    console.log(db);
 }
